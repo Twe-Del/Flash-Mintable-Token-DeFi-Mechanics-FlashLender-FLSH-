fd857b1bbe30181bfd10b859eee6e37cdcb68304
Twe-Del <twellebr1@outlook.com>
2026-03-06 15:19:09 +0400

    spadaro

---
+++ contracts/same transaction.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+
+contract FlashLender is ERC20, ERC20FlashMint {
+    constructor() ERC20("FlashLender", "FLSH") {
+        _mint(msg.sender, 5000000 * 10 ** decimals());
+    }
+}
